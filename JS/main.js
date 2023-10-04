6ecf9c04d718cbdbf2829d6b1e709e845e86e80a
Shohag <shoislamNYC@gmail.com>
2023-10-03 20:00:36 -0400

    Added win variable and win array and win function

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,24 @@
 /*----- constants -----*/
+    // Win Conditions
+    const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ];
 
 /*----- app's state (variables) -----*/
     let board;  
     let turn = 'X';
+    let win;
 
 /*----- cached element references -----*/
     const squares = Array.from(document.querySelectorAll('#board div'));
+    const messages = document.querySelector('h2');
 /*----- event listeners -----*/
     document.getElementById('board').addEventListener('click', handleTurn);
 
@@ -16,6 +29,8 @@
         board.forEach(function(mark, index){
         squares[index].textContent = mark;
         });
+        messages.textContent = `It's ${turn}'s turn!`;
+        messages.textContent = win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
     };
     // Initializing the board
     function init() {
@@ -31,8 +46,20 @@
         let idx = squares.findIndex(function(square) {
         return square === event.target;
         });
+        board[idx] = turn;
+        turn = turn === 'X' ? 'O' : 'X';
+        win = getWinner();
+        render();
     };
-   
+    // Handling Wins
+    function getWinner() {
+        let winner = null;
+        winningCombos.forEach(function(combo, index) {
+            if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) winner = board[combo[0]];
+          });
+          return winner;
+    };
+  
     
 
 /*----- MAIN-----*/
